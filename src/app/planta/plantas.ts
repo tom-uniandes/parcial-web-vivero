36a9a7b259fbf4e677b7af7218b64347611df5bf
Tomas Rodriguez <t.rodriguezh@uniandes.edu.co>
2023-04-30 12:00:35 -0500

    Add class plantas

---
+++ src/app/planta/plantas.ts
@@ -0,0 +1,20 @@
+export class Plantas {
+  id: number;
+  nombre_comun: string;
+  tipo: string;
+  clima: string
+
+
+  constructor(
+    id: number,
+    nombre_comun: string,
+    tipo: string,
+    clima: string
+  ) {
+    this.id = id;
+    this.nombre_comun = nombre_comun;
+    this.tipo = tipo;
+    this.clima = clima;
+  }
+
+}
