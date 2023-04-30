c944e1e83f051277a881d13cb9c98c0041f6a8a0
Tomas Rodriguez <t.rodriguezh@uniandes.edu.co>
2023-04-30 16:45:06 -0500

    Add tests

--- src/app/planta/planta-list/planta-list.component.spec.ts
+++ src/app/planta/planta-list/planta-list.component.spec.ts
@@ -2,16 +2,23 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { faker } from '@faker-js/faker';
 
 import { PlantaListComponent } from './planta-list.component';
+import { PlantaService } from '../planta.service';
+import { Planta } from '../planta';
 
 describe('PlantaListComponent', () => {
   let component: PlantaListComponent;
   let fixture: ComponentFixture<PlantaListComponent>;
+  let debug: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ PlantaListComponent ]
+      imports: [HttpClientModule],
+      declarations: [ PlantaListComponent ],
+      providers: [PlantaService]
     })
     .compileComponents();
   }));
@@ -19,10 +26,35 @@ describe('PlantaListComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(PlantaListComponent);
     component = fixture.componentInstance;
+
+    for(let i = 0; i < 3; i++) {
+      const planta = new Planta(
+        faker.datatype.number(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence(),
+      );
+      component.plantas.push(planta);
+    }
+
     fixture.detectChanges();
+    debug = fixture.debugElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should have 3 <tr class="lista-plantas"> elements', () => {
+    expect(debug.queryAll(By.css('tr.lista-plantas'))).toHaveSize(3)
+  });
+
+  it('should have 1 <tr class="encabezado-tabla"> elements', () => {
+    expect(debug.queryAll(By.css('tr.encabezado-tabla'))).toHaveSize(1)
+  });
+
+  it('should have 1 <div class="total-plantas> elements', () => {
+    expect(debug.queryAll(By.css('div.total-plantas'))).toHaveSize(1)
+  });
+
 });
--- src/app/planta/planta.service.spec.ts
+++ src/app/planta/planta.service.spec.ts
@@ -2,10 +2,12 @@
 
 import { TestBed, async, inject } from '@angular/core/testing';
 import { PlantaService } from './planta.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('Service: Planta', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
       providers: [PlantaService]
     });
   });
