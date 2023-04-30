51bdc379173873948b6d269a7e773cb527ab82ae
Tomas Rodriguez <t.rodriguezh@uniandes.edu.co>
2023-04-30 15:58:36 -0500

    Add html view to plant-list

--- src/app/planta/planta-list/planta-list.component.ts
+++ src/app/planta/planta-list/planta-list.component.ts
@@ -10,12 +10,36 @@ import { PlantaService } from '../planta.service';
 export class PlantaListComponent implements OnInit {
 
   plantas: Array<Planta> = [];
+  totalInterior: any;
+  totalExterior: any;
+
   constructor(private plantaService: PlantaService) { }
 
   getPlantas(): void {
     this.plantaService.getPlantas().subscribe((plantas) => {
       this.plantas = plantas;
+      this.getCantidadPlantasTipo();
     });
+  }
+
+  getCantidadPlantasTipo(): void {
+    let totalPlantas;
+    totalPlantas = this.plantas.length;
+
+    let totalInterior = 0;
+    let totalExterior = 0;
+
+    for(let contador = 0; totalPlantas>contador; contador++) {
+      let tipo = this.plantas[contador].tipo;
+      if(tipo == "Interior") {
+        totalInterior += 1;
+      } else if (tipo == "Exterior"){
+        totalExterior += 1;
+      }
+    }
+
+    this.totalInterior = totalInterior;
+    this.totalExterior = totalExterior;
   }
 
   ngOnInit() {
